151ce66dfd17d7f06b9262f373dc425a3873652e
stepan-volski <88038674+stepan-volski@users.noreply.github.com>
2021-11-03 21:11:02 +0300

    feat: add tasks 1-2

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const stream = new fs.ReadStream(path.join(__dirname + "/text.txt"));
+
+stream.on('readable', () => {
+    let data;
+    while (data = stream.read()) {
+        console.log(data.toString());
+      }
+});
---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const readline = require('readline');
+const filePath = path.join(__dirname + "/text.txt");
+let writeStream = fs.createWriteStream(filePath);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.setPrompt(`Hi! Please input your text: `);
+rl.prompt();
+
+rl.on('line', (input) => {
+    if (input === 'exit') {
+        endDialog();
+        return;
+    }
+    writeStream.write(input + "\n");
+});
+
+rl.on('close', () => endDialog());
+
+function endDialog() {
+    writeStream.end();
+    rl.close();
+    console.log("Dialog closed! Bye-bye!");
+}
