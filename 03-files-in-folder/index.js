489993d839226d8d554a707be26ebc3f1fdf073d
stepan-volski <88038674+stepan-volski@users.noreply.github.com>
2021-11-04 23:13:30 +0300

    feat: add task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const dir = path.join(__dirname + "/secret-folder");
+
+fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+  files.forEach(file => {
+    if (!file.isDirectory()) {
+      let filePath = path.join(dir + "/" + file.name);
+      fs.stat(filePath, (err, stats) => {
+        console.log(file.name.split(".")[0] + " - " + file.name.split(".")[1] + " - " + Math.ceil(stats.size / 1024) + "kb");
+      });
+    }
+  });
+});
