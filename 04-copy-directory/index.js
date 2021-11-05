57a3132fe9e6641e1f12bf7442a4a2695df69cdb
stepan-volski <88038674+stepan-volski@users.noreply.github.com>
2021-11-05 19:49:52 +0300

    feat: add task-4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const srcFolder = path.join(__dirname, "files");
+const destFolder = path.join(__dirname, "files-copy");
 
+fs.readdir(srcFolder, { withFileTypes: true }, (err, files) => {
+    fs.rmdir(destFolder, { recursive: true }, () => {
+        fs.mkdir(destFolder, { recursive: true }, () => {
+            files.forEach(file => {
+                let src = path.join(srcFolder, file.name);
+                let dest = path.join(destFolder, file.name);
+                fs.copyFile(src, dest, () => { });
+            })
+        });
+    });
+});
